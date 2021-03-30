80e843ec23dee91bc6e7625b723822920f8e3144
AntonKhabiuk <brotherosirus@gmail.com>
2021-03-30 16:54:34 +0300

    CC-12700 Introduce AvalaraTaxExtension module

---
+++ src/SprykerEco/Zed/AvalaraTax/Dependency/Plugin/CreateTransactionRequestAfterPluginInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Zed\AvalaraTax\Dependency\Plugin;
+
+use Generated\Shared\Transfer\AvalaraCreateTransactionResponseTransfer;
+use Generated\Shared\Transfer\CalculableObjectTransfer;
+
+/**
+ * Use this plugin to set taxes after `createTransaction` response is received from the Avalara Tax API.
+ */
+interface CreateTransactionRequestAfterPluginInterface
+{
+    /**
+     * Specification:
+     * - Executed after `createTransaction` response is received from the Avalara Tax API.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\CalculableObjectTransfer $calculableObjectTransfer
+     * @param \Generated\Shared\Transfer\AvalaraCreateTransactionResponseTransfer $avalaraCreateTransactionResponseTransfer
+     *
+     * @return \Generated\Shared\Transfer\CalculableObjectTransfer
+     */
+    public function execute(
+        CalculableObjectTransfer $calculableObjectTransfer,
+        AvalaraCreateTransactionResponseTransfer $avalaraCreateTransactionResponseTransfer
+    ): CalculableObjectTransfer;
+}
---
+++ src/SprykerEco/Zed/AvalaraTax/Dependency/Plugin/CreateTransactionRequestExpanderPluginInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Zed\AvalaraTax\Dependency\Plugin;
+
+use Generated\Shared\Transfer\AvalaraCreateTransactionRequestTransfer;
+use Generated\Shared\Transfer\CalculableObjectTransfer;
+
+/**
+ * Use this plugin to extend `createTransaction` request before it's is send to the Avalara Tax API.
+ */
+interface CreateTransactionRequestExpanderPluginInterface
+{
+    /**
+     * Specification:
+     * - Expands create transaction request with additional data.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\AvalaraCreateTransactionRequestTransfer $avalaraCreateTransactionRequestTransfer
+     * @param \Generated\Shared\Transfer\CalculableObjectTransfer $calculableObjectTransfer
+     *
+     * @return \Generated\Shared\Transfer\AvalaraCreateTransactionRequestTransfer
+     */
+    public function expand(
+        AvalaraCreateTransactionRequestTransfer $avalaraCreateTransactionRequestTransfer,
+        CalculableObjectTransfer $calculableObjectTransfer
+    ): AvalaraCreateTransactionRequestTransfer;
+}
